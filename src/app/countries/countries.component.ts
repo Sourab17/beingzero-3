dfb383cdb881414d1004dc095eec09a15d68bbb5
Sourab17 <sourab1799@gmail.com>
2020-01-08 14:43:17 +0530

    updated

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -8,11 +8,15 @@ import { DataService } from '../data.service';
 })
 export class CountriesComponent implements OnInit {
 countries;
+temp;
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getCountries().subscribe(d => { 
       this.countries = d;
+      this.temp=this.countries[104];
+      this.countries[104]=this.countries[0];
+      this.countries[0]=this.temp;
   })
 
 }
