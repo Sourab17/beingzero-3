e94045cf24c75e01b92d20072d3118344b376bae
Sourab17 <sourab1799@gmail.com>
2020-01-08 12:49:10 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CountriesComponent} from './countries/countries.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {ColorsComponent} from './colors/colors.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{path:'countries',component: CountriesComponent},{path:'home',component: HomeComponent},{path:'users',component: UsersComponent},{path:'colors',component: ColorsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
