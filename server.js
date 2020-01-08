275fbeeb5223934c8ce3d0c305c7b9c422270f49
Sourab17 <sourab1799@gmail.com>
2020-01-08 11:03:53 +0530

    checking

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname+'/dist/ngproject'));
+app.use(express.static(path.join(__dirname+'/dist/ngproject')));
 
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngproject/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngproject/index.html')));
 
 
 
